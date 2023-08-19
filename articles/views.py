69acaee3ec4f014be45e7b025acbabc311214a96
Mohammad <91499974+m15kh@users.noreply.github.com>
2023-08-20 01:45:25 +0330

    finished book!

--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,51 @@
-from typing import Optional
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-
-from django.views.generic import ListView , DetailView  
+from django.views import View
+from django.views.generic import ListView, DetailView, FormView # new
+from django.views.generic.detail import SingleObjectMixin # new
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
-from django.urls import reverse_lazy 
+from django.urls import reverse_lazy, reverse # new
+
+from .forms import CommentForm
 from .models import Article
+
+
+
+
+class CommentGet(DetailView): # new
+    model = Article
+    template_name = "article_detail.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+class CommentPost(SingleObjectMixin, FormView): # new
+    model = Article
+    form_class = CommentForm
+    template_name = "article_detail.html"
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.article = self.object
+        comment.save()
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        article = self.get_object()
+        return reverse("article_detail", kwargs={"pk": article.pk})
+
+
+
+class ArticleDetailView(LoginRequiredMixin, View): # new
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
+
+
 
 class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
@@ -14,6 +55,11 @@ class ArticleListView(LoginRequiredMixin, ListView):
 class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
 
 class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin , UpdateView):
     model = Article
@@ -44,3 +90,4 @@ class ArticleCreateView(LoginRequiredMixin, CreateView ):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
